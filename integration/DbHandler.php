925cc6971ab750fce27aa21dbb0c61e5b394670b
krish <krish@DESKTOP-4PR04U1>
2019-04-11 12:41:26 +0200

    Mijn eerste project heeft de tweede update gekregen

--- integration/DbHandler.php
+++ integration/DbHandler.php
@@ -16,20 +16,27 @@ $db = new DbHandler();
 $db->findWoord($woord);
 
 class DbHandler {
+    private $woord;
+    private $gevonden;
+    
     //put your code here
     public function findWoord($woord){
+        $this->woord = $woord;
         //Stap 1: Instellen van PDO
         $option = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,   
-        ];        
+        ];
+        
         $adres = '127.0.0.1';
         $charset = 'utf8mb4';
         $db = 'palindroom';
         $user = "root";
-        $password = "";        
-        $host = "mysql:host=$adres;dbname=$db;charset=$charset";       
+        $password = "";   
+        
+        $host = "mysql:host=$adres;dbname=$db;charset=$charset";  
+        
         $sql = "SELECT * FROM palindromen WHERE woord= '".$woord."';";
         try{
             //Stap 2: Connect met de database 
@@ -39,14 +46,18 @@ class DbHandler {
             
             // Stap 4: Connect met de database
             if ($stmt->rowCount () == 1){
-                echo "Woord is gevonden";
+                $this->gevonden = True;
             }
             else{
-                echo "Woord is niet gevonden";
+                $this->gevonden = False;
             }      
         }
         catch (PDOExeption $e) {
             echo "Ik ben Krishan Bhugwansing" . $e->getMassage()."(".$e->getCode().")." ;
         }        
+    }
+    
+    public function isWoordGevonden(){
+        return $this->gevonden;
     }
 }
