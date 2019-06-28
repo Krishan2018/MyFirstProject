bf9480f0186d60509b29ac56601d93e7349be77d
krish <krish@DESKTOP-4PR04U1>
2019-06-28 09:30:01 +0200

    Versie 1.0

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$viewData = array(
+    'palindroom' => "",
+    'message' => "Het omgedraaide woord heeft een betekenis",
+    'action' => "Vul een nieuw woord in of sluit de browser"           
+);
+include_once 'view/view.php';
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
